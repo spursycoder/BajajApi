7fb4a098dc4398d71b23b39aa4f7c6ea9251a687
spursycoder <74368103+spursycoder@users.noreply.github.com>
2023-03-31 11:53:01 +0530

    Add files via upload

---
+++ Stephno.js
@@ -0,0 +1,47 @@
+const express=require('express')
+const app = express();
+const PORT=4000;
+
+var uid="aastle_stephno_09052003"
+var roll="12017933"
+var mail="stephno2003@gmail.com"
+
+app.use(express.json())
+
+app.post('/hbfl',(req,res)=>{
+    console.log(req.body)
+    reqArray=req.body.data
+    var odd_arr=[]
+    var even_arr=[]
+    var alpha_arr=[]
+    reqArray.forEach(ele => {
+        if(ele>="A" && ele<="z"){
+            alpha_arr.push(ele)
+        }
+        else{
+            var num=Number(ele)
+            if(num%2==0){
+                even_arr.push(num.toString())
+            }
+            else{
+                odd_arr.push(num.toString())
+            }
+        }
+        
+    });
+
+    
+    res.status(200).send({
+        is_success: true,
+        user_id: uid,
+        email: mail,
+        roll_number: roll,
+        odd_numbers: odd_arr,
+        even_numbers: even_arr,
+        alphabets: alpha_arr
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log("listening on "+PORT)
+})
